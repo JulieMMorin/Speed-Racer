8b9e86c54b29dfdac61317a2b9b5b3ca16be3e6f
JulieMMorin <90783223+JulieMMorin@users.noreply.github.com>
2021-10-06 17:02:58 +0300

    Added carweight and enginetype

--- Assets/Scripts/SpeedRacer.cs
+++ Assets/Scripts/SpeedRacer.cs
@@ -2,7 +2,9 @@ using UnityEngine;
 
 public class SpeedRacer : MonoBehaviour
 {
-    string carModel = GTR R35
+    string carModel = GTR R35;
+    string engineType = V6, Twin Turbo;
+    int carWeight = 1609;
 
     // Start is called before the first frame update
     void Start()
