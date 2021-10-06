5bce018b67cb3653528341f3fb63a060da6da68f
JulieMMorin <90783223+JulieMMorin@users.noreply.github.com>
2021-10-06 17:04:13 +0300

    added yearMade

--- Assets/Scripts/SpeedRacer.cs
+++ Assets/Scripts/SpeedRacer.cs
@@ -5,6 +5,7 @@ public class SpeedRacer : MonoBehaviour
     string carModel = GTR R35;
     string engineType = V6, Twin Turbo;
     int carWeight = 1609;
+    int yearMade = 2009;
 
     // Start is called before the first frame update
     void Start()
