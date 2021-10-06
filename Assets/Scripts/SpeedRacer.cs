9921e96353d0f44fea76766ddc616cde0e1d7f7c
JulieMMorin <90783223+JulieMMorin@users.noreply.github.com>
2021-10-06 17:24:45 +0300

    added double quotes to string values

--- Assets/Scripts/SpeedRacer.cs
+++ Assets/Scripts/SpeedRacer.cs
@@ -2,8 +2,8 @@ using UnityEngine;
 
 public class SpeedRacer : MonoBehaviour
 {
-    string carModel = GTR R35;
-    string engineType = V6, Twin Turbo;
+    string carModel = "GTR R35";
+    string engineType = "V6, Twin Turbo";
     int carWeight = 1609;
     int yearMade = 2009;
     float maxAcceleration = 0.98;
@@ -13,7 +13,7 @@ public class SpeedRacer : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+     
     }
 
     // Update is called once per frame
