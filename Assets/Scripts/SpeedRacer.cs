f72ec7de81e5e9493d09a5016c0780288fe12869
JulieMMorin <90783223+JulieMMorin@users.noreply.github.com>
2021-10-06 18:29:43 +0300

    moved function, added semicolons,

--- Assets/Scripts/SpeedRacer.cs
+++ Assets/Scripts/SpeedRacer.cs
@@ -13,25 +13,26 @@ public class SpeedRacer : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        System.Console.WriteLine("RaceCar is a GTR R35. It has a V6 Twin Turbo Engine.");
+        print("RaceCar is a GTR R35. It has a V6 Twin Turbo Engine.");
 
         CheckWeight();
-        void CheckWeight()
-        { 
-            if (carWeight < 1500)
-                System.Console.WriteLine("The carModel weighs less than 1500kg.")
-             else if (carWeight > 1500)
-                System.Console.WriteLine("The carModel weighs more than 1500kg.")    
-                    
-                    }
-            
-     
+
+
+
 
     }
+    void CheckWeight()
+    {
+        if (carWeight < 1500)
+            print("The carModel weighs less than 1500kg.");
+        else if (carWeight > 1500)
+            print("The carModel weighs more than 1500kg.");
 
+
+    }
     // Update is called once per frame
     void Update()
     {
-       
+
     }
 }
